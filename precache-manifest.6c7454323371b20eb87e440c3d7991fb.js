467baa83b7be3bfa7d429d346891de030fcb3092
Code Sandbox <hello@codesandbox.io>
2021-11-30 01:02:15 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6c7454323371b20eb87e440c3d7991fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab33a78c4f6ea428b54901ec80c0e79",
+    "url": "https://emeritus101.github.io/csb-niz6d/index.html"
+  },
+  {
+    "revision": "b2aac58fade90e513e26",
+    "url": "https://emeritus101.github.io/csb-niz6d/static/js/2.117ab8b2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "https://emeritus101.github.io/csb-niz6d/static/js/2.117ab8b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5c5b645c4fed54c7d6",
+    "url": "https://emeritus101.github.io/csb-niz6d/static/js/main.49567a1a.chunk.js"
+  },
+  {
+    "revision": "f269139dfc62106fbced",
+    "url": "https://emeritus101.github.io/csb-niz6d/static/js/runtime-main.f0b60b66.js"
+  }
+]);
